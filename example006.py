d64ec351ad50035364394e6035dabdc54ac10141
Rob Liston <rliston@cisco.com>
2016-12-25 03:28:53 +0000

    added second plot

--- example006.py
+++ example006.py
@@ -1,10 +1,12 @@
 import urllib2
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import numpy as np
 print 'numpy ' + np.__version__
 import random 
 from scipy import stats
 import argparse
+import datetime
 
 # parse command line arguments
 parser = argparse.ArgumentParser()
@@ -19,17 +21,30 @@ ndays = len(spy) - args.horizon
 print 'ndays', ndays
 
 spy_r=[]
+act=[]
+date=[]
 for i in range(1, ndays):
     # Date,Open,High,Low,Close,Volume,Adj Close
     spy_r.append(float(spy[i].split(',')[4]) / float(spy[i+args.horizon].split(',')[4]) - 1)
+    act.append(float(spy[i].split(',')[4]))
+    date.append(datetime.datetime.strptime(spy[i].split(',')[0], "%Y-%m-%d").date())
 
 x = np.array(spy_r, dtype='float')
 print 'x.shape', x.shape
-
 y = np.random.normal(np.mean(x), np.std(x), 100000)
 print 'y.shape', y.shape
+z = np.array(act, dtype='float')
+print 'z.shape', z.shape
 
-plt.hist(y, 100, normed=1, facecolor='green', alpha=0.75)
-plt.hist(x, 100, normed=1, facecolor='red', alpha=0.50)
-plt.title(vars(args))
+fig, ax = plt.subplots(2, 1, sharex=False, sharey=False)
+
+ax[0].hist(y, 100, normed=1, facecolor='green', alpha=0.75)
+ax[0].hist(x, 100, normed=1, facecolor='red', alpha=0.50)
+ax[0].set_title(vars(args))
+
+ax[1].fmt_xdata = mdates.DateFormatter('%Y-%m-%d')
+ax[1].plot(date,z,color='k')
+ax2 = ax[1].twinx()
+ax2.plot(date,x,color='r')
+
 plt.show()
