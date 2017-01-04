e5fb0c03e6af42f123b2c1d53aee8fe1e75e0aa1
Rob Liston <rliston@cisco.com>
2017-01-04 23:35:21 +0000

    print gradient norm

--- example009.py
+++ example009.py
@@ -45,17 +45,42 @@ print 'pi.shape',pi.shape
 input_var = T.matrix(dtype=theano.config.floatX)
 target_var = T.vector(dtype='int32')
 network = L.layers.InputLayer((None, args.window), input_var)
-network = L.layers.DenseLayer(network, 1000)
-network = L.layers.DenseLayer(network, 100)
-network = L.layers.DenseLayer(network, 100)
-network = L.layers.DenseLayer(network, 100)
-network = L.layers.DenseLayer(network, 10)
-network = L.layers.DenseLayer(network, 2, nonlinearity=L.nonlinearities.softmax)
+print 'input', L.layers.get_output_shape(network)
+network = L.layers.ReshapeLayer(network, ((-1, 1, args.window)))
+print 'reshape', L.layers.get_output_shape(network)
+network = L.layers.Conv1DLayer(network,num_filters=256,filter_size=11,stride=2)
+print 'conv', L.layers.get_output_shape(network)
+network = L.layers.Conv1DLayer(network,num_filters=256,filter_size=11,stride=2)
+print 'conv', L.layers.get_output_shape(network)
+network = L.layers.Conv1DLayer(network,num_filters=256,filter_size=11,stride=2)
+print 'conv', L.layers.get_output_shape(network)
+network = L.layers.Conv1DLayer(network,num_filters=256,filter_size=11,stride=2)
+print 'conv', L.layers.get_output_shape(network)
+conv = L.layers.Conv1DLayer(network,num_filters=256,filter_size=11,stride=2)
+print 'conv', L.layers.get_output_shape(conv)
+gap = L.layers.Pool1DLayer(conv, pool_size=L.layers.get_output_shape(conv)[2], stride=None, pad=0, mode='average_inc_pad')
+print 'gap', L.layers.get_output_shape(gap)
+network = L.layers.DenseLayer(gap, 2, nonlinearity=L.nonlinearities.softmax)
+print 'output', L.layers.get_output_shape(network)
+
+#input_var = T.matrix(dtype=theano.config.floatX)
+#target_var = T.vector(dtype='int32')
+#network = L.layers.InputLayer((None, args.window), input_var)
+#network = L.layers.DenseLayer(network, 10000)
+#network = L.layers.DenseLayer(network, 1000)
+#network = L.layers.DenseLayer(network, 1000)
+#network = L.layers.DenseLayer(network, 1000)
+#network = L.layers.DenseLayer(network, 1000)
+#network = L.layers.DenseLayer(network, 1000)
+#network = L.layers.DenseLayer(network, 100)
+#network = L.layers.DenseLayer(network, 2, nonlinearity=L.nonlinearities.softmax)
+
 prediction = L.layers.get_output(network)
 loss = L.objectives.aggregate(L.objectives.categorical_crossentropy(prediction, target_var), mode='mean')
 params = L.layers.get_all_params(network, trainable=True)
 updates = L.updates.adam(loss, params, learning_rate=args.lr)
-train_fn = theano.function([input_var, target_var], loss, updates=updates)
+scaled_grads,norm = L.updates.total_norm_constraint(T.grad(loss,params), np.inf, return_norm=True)
+train_fn = theano.function([input_var, target_var], [loss,norm], updates=updates)
 test_fn = theano.function([input_var], L.layers.get_output(network, deterministic=True))
 
 d = np.empty([args.batchsize,args.window],dtype='float32')
@@ -64,20 +89,25 @@ t0 = time.time()
 t = time.time()
 for i in range(args.nepoch):
     tloss=0
+    tnorm=0
     #train
     for j in range(args.nbatch):
         for k in range(args.batchsize):
-            w = np.random.randint(int(pi.shape[0]*args.test),pi.shape[0]-args.window)
+            #w = np.random.randint(int(pi.shape[0]*args.test),pi.shape[0]-args.window)
+            w = np.random.randint(0,int(pi.shape[0]*(1-args.test))-args.window)
             d[k] = pi[w:w+args.window]
             if np.random.randint(0,2)==0:
                 l[k]=0
             else:
                 np.random.shuffle(d[k])
                 l[k]=1
-        tloss += train_fn(d,l)
+        bloss,bnorm = train_fn(d,l)
+        tloss += bloss
+        tnorm += bnorm
     #test 
     for k in range(args.batchsize):
-        w = np.random.randint(0,int(pi.shape[0]*args.test-args.window))
+        #w = np.random.randint(0,int(pi.shape[0]*args.test-args.window))
+        w = np.random.randint(int(pi.shape[0]*(1-args.test)),pi.shape[0]-args.window)
         d[k] = pi[w:w+args.window]
         if np.random.randint(0,2)==0:
             l[k]=0
@@ -88,5 +118,5 @@ for i in range(args.nepoch):
     val_predictions = np.argmax(val_output, axis=1)
     tacc = np.mean(val_predictions == l)
 
-    print 'epoch {:4d} loss {:12.8f} accuracy {:12.8f} n_zero {:6d} n_one {:6d} t_epoch {:4d} t_total {:4d}'.format(i, tloss/args.nbatch, tacc, np.sum(val_predictions==0), np.sum(val_predictions==1), int(time.time()-t), int(time.time()-t0))
+    print 'epoch {:8d} loss {:12.8f} grad {:12.8f} accuracy {:12.8f} n_zero {:6d} n_one {:6d} t_epoch {:4d} t_total {:8d}'.format(i, tloss/args.nbatch, tnorm/args.nbatch, tacc, np.sum(val_predictions==0), np.sum(val_predictions==1), int(time.time()-t), int(time.time()-t0))
     t = time.time()
